44d6b3d4394503560a6bc68b3f057f68de9bbe46
mattesja <mattesjam@googlemail.com>
2012-04-15 06:10:52 -0700

    cmdline

--- mp3dirtag.scala
+++ mp3dirtag.scala
@@ -1,9 +1,12 @@
 import org.jaudiotagger.audio._
 import org.jaudiotagger.tag._
 import java.io.File
+import java.util.HashMap
 import scala.collection.JavaConversions._
 
 object mp3 {
+  type OptionMap = Map[Symbol,String]
+
   def getFiles(file: File): List[AudioFile] = {
     file match {
       case f: File if f.isFile && (f.getName.endsWith(".mp3") || f.getName.endsWith("m4a")) => List(AudioFileIO.read(file))
@@ -12,28 +15,51 @@ object mp3 {
     }
   }
 
-  def writeTag(audioFile: AudioFile, genre: String, artist: String) {
+  def writeTag(audioFile: AudioFile, options: OptionMap) {
     val file = audioFile.getFile
     val fileName = file.getName
     val title = fileName.substring(0, fileName.length - 4)
     val composer = file.getParentFile.getParentFile.getName
     val album = composer.split(" ")(0)+" - "+file.getParentFile.getName
+    val artist = options.getOrElse('artist, "-")
+    val genre = options.getOrElse('genre, "-")
     
-    println("title: "+title+", album: "+album+", composer: "+composer+", genre: "+genre+", artist: "+artist)
+    println("title: "+title+", album: "+album+" composer: "+composer)
     val tag: Tag = audioFile.getTagOrCreateAndSetDefault
     tag.setField(FieldKey.TITLE,title)
     tag.setField(FieldKey.ALBUM,album)
     tag.setField(FieldKey.COMPOSER,composer)
-    tag.setField(FieldKey.GENRE,genre)
     tag.setField(FieldKey.TRACK,title.substring(0,2))
-    if (artist !=null && artist != "") {
+    if (genre != "-") {
+      tag.setField(FieldKey.GENRE,genre)
+    }
+    if (artist != "-") {
       tag.setField(FieldKey.ARTIST,artist)
     }
     audioFile.commit()
   }
 
   def main(args: Array[String]) {
-    val files = getFiles(new File(args(0)))
-    files.foreach(x => writeTag(x, args(1), args(2)))
+    val usage = """
+    Usage: mp3 [-artist value] [-genre value] directory
+    """  
+    if (args.length == 0) println(usage)
+    
+    def nextOption(map : OptionMap, list: List[String]) : OptionMap = {
+      def isSwitch(s : String) = (s(0) == '-')
+	list match {
+          case Nil => map
+          case "-genre" :: value :: tail => nextOption(map ++ Map('genre -> value), tail)
+          case "-artist" :: value :: tail => nextOption(map ++ Map('artist -> value), tail)
+          case string :: Nil =>  nextOption(map ++ Map('directory -> string), list.tail)
+          case option :: tail => println("Unknown option '" + option + "'\n" + usage) 
+                                 sys.exit(1) 
+	}
+    }
+    val options = nextOption(Map(),args.toList)
+
+    val files = getFiles(new File(options('directory)))
+    println("Settings tags in "+files.size+" files in directory "+options('directory))
+    files.foreach(x => writeTag(x, options))
   }
 }
